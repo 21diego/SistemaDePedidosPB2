a1c6c2fb2ea4df5e8552707f5a702eb6fd3e9c7c
21diego <morinigodiego21@gmail.com>
2019-11-18 01:01:50 -0300

    probando prueba

--- SistemaDePedidosPB2/test/src/TestProducto.java
+++ SistemaDePedidosPB2/test/src/TestProducto.java
@@ -7,8 +7,24 @@ import org.junit.Test;
 public class TestProducto {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void creacionDeObjetoCorrecto() {
+		String nombreProd = "Milanesa de Carne";
+		String descProd = "Filete de carne vacuna, pasado por huevo batido y luego por pan rallado";
+		Double precioProd = 75.0;
+		Integer codProd = 01;
+		String codEspProd = "P01";
+		
+		Producto prod = new Producto(nombreProd,descProd,precioProd,codProd);
+		
+		//Mostrar en consola
+		System.out.println(prod);
+		
+		//Verificacion de los datos
+		assertTrue(prod.getNombre().equals(nombreProd));
+		assertTrue(prod.getDescripcion().equals(descProd));
+		assertTrue(prod.getPrecio().equals(precioProd));
+		assertTrue(prod.getId().equals(codEspProd));
+		
 	}
 
 }
